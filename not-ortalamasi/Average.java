a958d516b65515602982eb4272b82c44906f340b
Yunus Atahan <yunus10atahan@gmail.com>
2021-05-16 16:57:52 +0300

    ortalama uyulamasi tamalandi.

--- not-ortalamasi/Average.java
+++ not-ortalamasi/Average.java
@@ -3,7 +3,27 @@ import java.util.Scanner;
 public class Average {
 
 	public static void main(String[] args){
-		System.out.println("test");				
+		Scanner scanner = new Scanner(System.in);
+
+		int fiz, kim, turk, tar, muz;
+		System.out.print("Fizik notu:");
+		fiz = scanner.nextInt();
+
+		System.out.print("Kimya notu:");
+		kim = scanner.nextInt();
+
+		System.out.print("Turkce notu:");
+		turk = scanner.nextInt();
+
+		System.out.print("Tarih notu:");
+		tar = scanner.nextInt();
+
+		System.out.print("Muzik notu:");
+		muz = scanner.nextInt();
+
+		double average = (fiz+kim+turk+tar+muz) / 5.0;
+		System.out.println("Ortalama: " + average);
+		System.out.println(average > 60 ? "Sinifi Gecti" : "Sinifta Kaldi");
 	}
 
 }
