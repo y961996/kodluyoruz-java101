aa22f7e0a278a8f6072d5560861f2f3b16a24bac
Yunus Atahan <yunus10atahan@gmail.com>
2021-05-31 19:30:45 +0300

    getInput metotu eklendi.

--- Bitirme-Projesi-Mayin-Tarlasi/MayinTarlasi.java
+++ Bitirme-Projesi-Mayin-Tarlasi/MayinTarlasi.java
@@ -1,17 +1,41 @@
+import java.util.Scanner;
+
 public class MayinTarlasi{
-  public String[][] tarla;
-  public int mayinSayisi;
+  private String[][] tarla;
+  private int mayinSayisi;
+  private Scanner scanner;
+
+  // Burada ki metotları private yap
 
   public MayinTarlasi(int row, int column){
+    this.scanner = new Scanner();
+
     this.tarla = new String[row][column];
     this.mayinSayisi = row * column / 4;
   }
 
   public void run(){
-    System.out.println("TEST");
+    System.out.println("Mayin Tarlasi Oyununa Hosgeldiniz !");
+    mayinTarlasiniEkranaYazdir();
+    int[] satirSutun = getInput();
+    chechkCoordinate(satirSutun[0], satirSutun[1]);
+  }
+
+  public int[] getInput(){
+    int[] satirSutun = new int[2];
+    
+    System.out.print("Satir Giriniz : ");
+    satirSutun[0] = scanner.nextInt();
+    System.out.print("Sutun giriniz : ");
+    satirSutun[1] = scanner.nextInt();
+    System.out.println("==================================");
+
+    return satirSutun;
   }
 
   public void initializeTarla(){}
+
+  public boolean chechkCoordinate(int satir, int sutun){return false;}
 
   public void mayinTarlasiniEkranaYazdir(){}
 
